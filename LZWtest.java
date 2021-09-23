fadd27ad5366b545ff6fc7df4d9cbb5953a6af40
rhea <rhea0866@gmail.com>
2021-09-22 17:25:19 -0700

    Prints out execution time

--- LZWtest.java
+++ LZWtest.java
@@ -4,15 +4,11 @@ public class LZWtest {
 	public static void main (String [] args) throws FileNotFoundException
 	{
 	LZW2 hello = new LZW2("text.txt");
-	long startTime, endTime;
 //	System.out.println(hello.full()); 
 	hello.compress();
-	 startTime = System.currentTimeMillis();
+	long startTime = System.nanoTime();
 	hello.decode();
-	endTime = System.currentTimeMillis();
-	float time  = (endTime- startTime);
-	System.out.println("sup");
-	System.out.println(time);
+	long endTime = System.nanoTime();
 	
 	
 	//224 //then 228
@@ -20,6 +16,10 @@ public class LZWtest {
 	//time without changes 1506250/ 1308208 / 1314791
 //	with chnages 1200708 and 1043167 so overall lower times ??
 	
+    long timeElapsed = endTime - startTime;
+
+    System.out.println("Execution time in nanoseconds: " + timeElapsed);
+    System.out.println("Execution time in milliseconds: " + timeElapsed / 1000000);
 
 	}
 }
